eb9e9677c93f69d62bd3092b748cade48070c835
Caelin Jackson-King <caelink@gmail.com>
2018-11-18 16:12:34 -0800

    Changed my mind about what an initializer anti pattern is

--- Source/New View.xctemplate/___VARIABLE_filePrefix___ViewController.swift
+++ Source/New View.xctemplate/___VARIABLE_filePrefix___ViewController.swift
@@ -5,10 +5,15 @@ import UIKit
 class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
     var myView: ___VARIABLE_filePrefix___View?
     
-    convenience init() {
+    init() {
         // Boilerplate 'empty' initialize to save other boilerplate
-        self.init(nibName: nil, bundle: nil)
+        super.init(nibName: nil, bundle: nil)
         myView = ___VARIABLE_filePrefix___View(delegate: self)
+    }
+    
+    // Least boilerplate without using initializer antipatterns
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     override func loadView() {
